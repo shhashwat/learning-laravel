4e01fc43c4d6e7519fe358e74b1ba64ad2dbfab1
Shashwat Singh <sshashwatssingh@gmail.com>
2025-04-01 15:34:21 +0530

    day 6 done

--- example/resources/views/jobs.blade.php
+++ example/resources/views/jobs.blade.php
@@ -7,10 +7,10 @@
     </x-slot:heading>
     <p  class="text-2xl font-medium flex justify-center items-center">Hello from the jobs page</p>
 
-    <ul>
+    <ul class="list-disc list-inside">
         @foreach ($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}">
-                <li> <strong>{{ $job['title'] }}</strong> : Pays {{ $job['salary'] }} per year </li>
+                <li class="text-blue-600 hover:underline"> <strong>{{ $job['title'] }}</strong> : Pays {{ $job['salary'] }} per year </li>
             </a>
         @endforeach
     </ul>
