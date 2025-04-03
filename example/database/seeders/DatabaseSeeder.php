041ba094779f3f82a68084246eb5e57b52680c17
Shashwat Singh <sshashwatssingh@gmail.com>
2025-04-03 11:42:44 +0530

    Day 15 done

--- example/database/seeders/DatabaseSeeder.php
+++ example/database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)->create();
+
+        $this->call(JobSeeder::class);
+
+        $this->call(PostSeeder::class);
     }
 }
